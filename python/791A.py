b7a0fc8d0be2cf8aa864dcea1bef82db4fa4df92
overseerboots <github.earwig416@passinbox.com>
2024-11-24 21:25:07 +0000

    "Improve" 791A.py

--- python/791A.py
+++ python/791A.py
@@ -3,15 +3,16 @@ https://codeforces.com/contest/791/problem/A
 Accepted
 77ms
 28kb
-2024-10-14 20:12:26
+https://codeforces.com/contest/791/problem/A
 '''
 
-bear_weights = input().split(" ")
 years = 0
-limak = int(bear_weights[0])
-bob = int(bear_weights[1])
-while limak <= bob:
-    limak = limak * 3
-    bob = bob * 2
-    years = years + 1
+
+brothers = input().split(" ")
+limak, bob = int(brothers[0]), int(brothers[1])
+del brothers
+while bob >= limak:
+	bob *= 2
+	limak *= 3
+	years += 1
 print(years)
